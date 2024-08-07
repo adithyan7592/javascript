a4315e4029f0ba1cedfd5793853ef7af0b23a563
adithyan7592 <adhisreeji8@gmail.com>
2024-08-07 16:03:58 +0530

    fizzbuzz

--- js/custom.js
+++ js/custom.js
@@ -38,6 +38,69 @@ else if(mark>30){
 // else{
 //     alert('fail')
 // }
-fno=parseFloat(prompt("enter the first number"))
-sno=parseFloat(prompt("enter the second number"))
-alert(fno+sno)
+// fno=parseFloat(prompt("enter the first number"))
+// sno=parseFloat(prompt("enter the second number"))
+// alert(fno+sno)
+// op=6
+// switch(op){
+//     case 1:console.log("sunday");
+//     break;
+//     case 2:console.log("monday");
+//     break;
+//     case 3:console.log("tuesday");
+//     break;
+//     default:console.log("invalid");
+//     break;
+// }
+// i=1
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+// i=1
+// do{
+//     console.log(i);
+//     i=i+2;
+// }while(i<=10)
+//  for(i=1;i<=100;i++)
+//     {
+//     if(i%3==0){
+//         console.log("fiz");
+//     }
+//     else{
+//         console.log(i);
+//     }
+//     }   
+// for(i=1;i<=100;i++)
+//     {
+//         if(i%5==0){
+//             console.log("buzz");
+//         }
+//         else{
+//             console.log(i)
+//         }
+//     }
+    // for(i=1;i<=100;i++)
+    //     {
+    //         if(i%5==0&&i%3==0){
+    //             console.log(" fizzbuzz");
+    //         }
+    //         else{
+    //             console.log(i)
+    //         }
+    //     }
+    for(i=1;i<=100;i++)
+        {
+            if(i%3==0){
+                console.log("fizz");
+            }
+            else if(i%5==0){        
+                console.log("buzz");
+            }
+            else if(i%5==0&&i%3==0){
+                console.log("fizzbuzz");
+            }
+            else{
+                console.log(i);
+            }
+        }
