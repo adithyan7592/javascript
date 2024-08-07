8b33c73cc0162173154386ae7f54e17b7d8aa851
adithyan7592 <adhisreeji8@gmail.com>
2024-08-07 21:34:30 +0530

    fizzbizz

--- js/custom.js
+++ js/custom.js
@@ -91,15 +91,17 @@ else if(mark>30){
     //     }
     for(i=1;i<=100;i++)
         {
-            if(i%3==0){
+            if(i%5==0&&i%3==0){
+                console.log("fizzbuzz");
+            }
+            else if(i%3==0){
                 console.log("fizz");
             }
             else if(i%5==0){        
                 console.log("buzz");
             }
-            else if(i%5==0&&i%3==0){
-                console.log("fizzbuzz");
-            }
+            
+            
             else{
                 console.log(i);
             }
