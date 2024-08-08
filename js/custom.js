debf96e26fec87d14e23cb4a9204563a4d048265
adithyan7592 <adhisreeji8@gmail.com>
2024-08-08 16:05:36 +0530

    edited

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,35 @@
-a=10
-b=10
-c=20
-console.log(a+b);
-console.log(a+c);
-console.log(a==b&&b==c);
-console.log(+"5");
-console.log(5+ +"5");
-console.log(a+=b);
-console.log(!false);
-console.log(a==b||a==c);
-console.log(a*b);
-mark=93
-if(mark>90){
-    console.log("A+");
-}
+// a=10
+// b=10
+// c=20
+// console.log(a+b);
+// console.log(a+c);
+// console.log(a==b&&b==c);
+// console.log(+"5");
+// console.log(5+ +"5");
+// console.log(a+=b);
+// console.log(!false);
+// console.log(a==b||a==c);
+// console.log(a*b);
+// mark=93
+// if(mark>90){
+//     console.log("A+");
+// }
 
-else if(mark>70){
-    console.log("A")
-}
-else if(mark>60){
-    console.log("B+")
-}
-else if(mark>50){
-    console.log("B")
-}
-else if(mark>40){
-    console.log("C+")
-}
-else if(mark>30){
-    console.log("FAILED");
-}
+// else if(mark>70){
+//     console.log("A")
+// }
+// else if(mark>60){
+//     console.log("B+")
+// }
+// else if(mark>50){
+//     console.log("B")
+// }
+// else if(mark>40){
+//     console.log("C+")
+// }
+// else if(mark>30){
+//     console.log("FAILED");
+// }
 
 // score=prompt("enter your score")
 // if(score>90){
@@ -89,20 +89,91 @@ else if(mark>30){
     //             console.log(i)
     //         }
     //     }
-    for(i=1;i<=100;i++)
-        {
-            if(i%5==0&&i%3==0){
-                console.log("fizzbuzz");
-            }
-            else if(i%3==0){
-                console.log("fizz");
-            }
-            else if(i%5==0){        
-                console.log("buzz");
-            }
+    // for(i=1;i<=100;i++)
+    //     {
+    //         if(i%5==0&&i%3==0){
+    //             console.log("fizzbuzz");
+    //         }
+    //         else if(i%3==0){
+    //             console.log("fizz");
+    //         }
+    //         else if(i%5==0){        
+    //             console.log("buzz");
+    //         }
             
             
-            else{
-                console.log(i);
-            }
+    //         else{
+    //             console.log(i);
+    //         }
+    //     }
+
+    // for(i=1;i<=10;i++){
+    //     if(i==5){
+    //     break;
+    //   }
+    // console.log(i);
+        
+    // }
+    // for(i=1;i<=10;i++){
+    //     if(i==5){
+    //     continue;
+    //     }
+    // console.log(i);
+        
+    // }
+    // function asd(){
+
+    //     if(true){
+    //         let a=10
+    //         console.log("inside block"+a);
+    //     }
+    //     console.log("inside function"+a);
+    // }
+    // asd()
+    // function asd(){
+
+    //     if(true){
+    //         var a=10
+    //         console.log("inside block"+a);
+    //     }
+    //     console.log("inside function"+a);
+    // }
+    // asd()
+    // function asd(){
+
+    //     if(true){
+    //         var a=10
+    //         console.log("inside block"+a);
+    //     }
+    //     console.log("inside function"+a);
+    // }
+    // asd()
+    // console.log("outside function"+a);
+    // for(i=1;i<=5;i++){
+    //     str=""
+    // for(j=1;j<=i;j++){
+    //     str+=j
+    // }
+    // console.log(str);
+    // }
+    // for(i=1;i<=5;i++){
+    //     str=""
+    // for(j=1;j<=i;j++){
+    //     str+="*"
+    // }
+    // console.log(str);
+    // }
+    // for(i=1;i<=5;i++){
+    //     str=""
+    // for(j=5;j>=1;j--){
+    //     str+=j
+    // }
+    // console.log(str);
+    // }
+    for(i=5;i>=1;i--){
+        str=""
+        for(j=5;j>=i;j--){
+            str+=j
         }
+        console.log(str);
+    }
