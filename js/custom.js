f6617107bbbd0cb5c7eebd686a86390059e04e1d
adithyan7592 <adhisreeji8@gmail.com>
2024-08-09 12:42:17 +0530

    nested forloop

--- js/custom.js
+++ js/custom.js
@@ -170,10 +170,34 @@
     // }
     // console.log(str);
     // }
-    for(i=5;i>=1;i--){
+    // for(i=5;i>=1;i--){
+    //     str=""
+    //     for(j=5;j>=i;j--){
+    //         str+=j
+    //     }
+    //     console.log(str);
+    // }
+    // for(i=1;i<=5;i++){
+    //     str=""
+    //     for(j=5;j>=i;j--){
+    //         str+=j
+    //     }
+    //     console.log(str);
+    // }
+    // for(i=5;i>=1;i--){
+    //     str=""
+    //     for(j=1;j<=i;j++){
+    //         str+=j
+    //     }
+    //     console.log(str);
+    // }
+    num=1
+    for(i=1;i<=4;i++){
         str=""
-        for(j=5;j>=i;j--){
-            str+=j
+        for(j=0;j<i;j++){
+            str+=num
+            num++
         }
         console.log(str);
-    }
+    } 
+    
